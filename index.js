b6f40d712a049871eb7d050f146c10aef2995a97
syedaumema ahmed <umemaahmed009@gmail.com>
2024-03-15 15:08:20 +0500

    Deleted unnecessary node modules

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const radiologistRouter = require('./Radiologist/route');
+const radiologistRouter = require('./Route/radiologistroute');
+const patientRouter=require('./Route/patientroute');
+const xRayRouter = require("./Route/xRayRoute")
+const reportRouter = require("./Route/reportRoute")
+require('dotenv').config()
+
 const port = 3000;
-const url = "mongodb+srv://aizakhank10000:dingdong123@cluster0.qhx8m7o.mongodb.net/?retryWrites=true&w=majority"
+const url = process.env.DB_URI
 
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 // Connect to your Atlas cluster
 // const client = new MongoClient(url);
 
-require('dotenv').config()
 // console.log(process.env)
 
 app.use(bodyParser.urlencoded({extended : true}));
@@ -21,22 +25,19 @@ app.use(cookieParser());
 app.use(cors());
 
 mongoose.connect(url)
-  .then(() => console.log('Connected!'));
+  .then(() => console.log('MongoDB Atlas is connected!'));
   
-// app.use("/radiologist", customerRouter);
-
-// app.listen(port, ()=> {
-//     console.log("app is working on " ,port)
-// })
-
-// mongodb+srv://shassan22750:VllKbYAbC7La3Rqz@cluster0.9vyc4m5.mongodb.net/?retryWrites=true&w=majority
+app.use("/radiologist", radiologistRouter);
+app.use("/patient",patientRouter);
+app.use("/xray",xRayRouter);
+app.use("/report",reportRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`App is listeining on ${port}`);
+  console.log(`App is listening on port ${port}`);
 });
 
 app.use(bodyParser.json());
