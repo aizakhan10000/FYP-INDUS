1389ecd14cc631da9e82f4d6a6535759ed85dfc2
Kundan-404 <kundanmalhi45@gmail.com>
2024-03-17 00:07:45 +0500

    front-end

---
+++ x-ray-auto-detection/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import LoginPage from './LoginPage'; // Adjust the import path as necessary
+import Dashboard from './Dashboard';
+import Patients from './Patients';
+import PatientDetails from './PatientDetail';
+import GenerateReport from './GenerateReport';
+import PatientHistory from './PatientHistory';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/patient-detail" element={<PatientDetails />} />
+          <Route path="/patient-history" element={<PatientHistory />} />
+          <Route path="/report" element={<GenerateReport />} />
+          {/* You can add more Route components here for other paths */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
